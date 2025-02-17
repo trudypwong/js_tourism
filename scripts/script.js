3c3bc3f2d01e7b613592ed3b224852d97cae16b6
Trudy Wong <trudypwong@gmail.com>
2025-02-16 18:34:22 -0800

    Extra activity details from json on page load

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,68 @@
 document.addEventListener('DOMContentLoaded', function () {
   // Function to load initial activity list from json
-  function loadActivityList() {}
+  function loadActivityList() {
+    const list = document.querySelector('.activity-list');
+    fetch('./../data/activities.json')
+      .then((response) => response.json())
+      .then((json) => {
+        return json.map((item) => {
+          const card = document.createElement('a');
+          card.classList.add('activity-card');
+          const article = document.createElement('article');
+          //  image
+          const image = document.createElement('img');
+          image.setAttribute('src', item.img);
+          image.setAttribute('alt', item.alt);
+          // main contents
+          const container = document.createElement('div');
+          container.classList.add('activity-name');
+          const name = document.createElement('h3');
+          name.textContent = item.name;
+          const tag = document.createElement('p');
+          tag.classList.add('tag');
+          tag.textContent = item.tag;
+          container.appendChild(name);
+          container.appendChild(tag);
+          // Also save some other information - hidden on main page
+          const hidden = document.createElement('div');
+          hidden.classList.add('hidden');
+          const description = document.createElement('p');
+          description.classList.add('activity-description');
+          description.textContent = item.description;
+          const date = document.createElement('p');
+          date.classList.add('activity-date');
+          date.textContent = item.date;
+          const time = document.createElement('p');
+          time.classList.add('activity-time');
+          time.textContent = item.time;
+          hidden.appendChild(description);
+          hidden.appendChild(date);
+          hidden.appendChild(time);
+          article.appendChild(hidden);
+          // append all the information
+          article.appendChild(image);
+          article.appendChild(container);
+          card.appendChild(article);
+          // add it to the DOM list
+          // list.appendChild(card);
+          return card;
+        });
+      })
+      .then((cards) => {
+        return cards.forEach((card) => {
+          list.appendChild(card);
+        });
+      })
+      .then((response) => {
+        setupShowActivityDetail();
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
 
   // Function to handle clicking an activity card
+  // This needs to be called after the content is dynamically added
   function setupShowActivityDetail() {
     const activities = document.querySelectorAll('.activity-card');
     activities.forEach((activity) => {
@@ -11,18 +71,29 @@ document.addEventListener('DOMContentLoaded', function () {
         // Extract the values
         const name = e.target.querySelector('.activity-name h3')?.textContent;
         const tag = e.target.querySelector('.activity-name .tag')?.textContent;
+        const description = e.target.querySelector(
+          '.activity-description'
+        )?.textContent;
+        const date = e.target.querySelector('.activity-date')?.textContent;
+        const time = e.target.querySelector('.activity-time')?.textContent;
         const image = e.target.querySelector('img')?.getAttribute('src');
+        const alt = e.target.querySelector('img')?.getAttribute('alt');
+        console.log(name, tag, image, date, time, description);
 
-        console.log(name);
-        console.log(tag);
-        console.log(image);
         // Update the values
         const nameDOM = document.querySelector('.a-detail-name');
         const tagDOM = document.querySelector('.a-detail-tag');
         const imageDOM = document.querySelector('.a-detail-image');
+        const descDOM = document.querySelector('.a-detail-desc');
+        const dateDOM = document.querySelector('.a-detail-date');
+        const timeDOM = document.querySelector('.a-detail-time');
         nameDOM.textContent = name;
         tagDOM.textContent = tag;
         imageDOM.setAttribute('src', image);
+        imageDOM.setAttribute('alt', alt);
+        descDOM.textContent = description;
+        dateDOM.textContent = date;
+        timeDOM.textContent = time;
 
         // Show the popup
         const detailContainer = document.querySelector('.detail-content');
@@ -84,6 +155,6 @@ document.addEventListener('DOMContentLoaded', function () {
   // Initial setup based on page content
   setupNavbarToggle();
   setupEventPageNavigation();
-  setupShowActivityDetail();
   loadActivityList();
+  // setupShowActivityDetail();
 });
